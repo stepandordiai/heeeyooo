03f95813376b6adb9a2abf6b8b84687a01643da3
Stepan Dordiai <stepandordiai@gmail.com>
2025-09-18 18:05:28 +0200

    Update not-found page

--- src/app/not-found/page.tsx
+++ src/app/not-found/page.tsx
@@ -1,5 +1,20 @@
-const NotFound = () => {
-	return <h1>Not Found Page</h1>;
-};
+// app/not-found/page.tsx
+import Link from "next/link";
 
-export default NotFound;
+export default function NotFound() {
+	return (
+		<div style={{ textAlign: "center", marginTop: "10rem" }}>
+			<h1 style={{ fontSize: "5rem", marginBottom: "1rem" }}>404</h1>
+			<p style={{ fontSize: "1.5rem", marginBottom: "2rem" }}>
+				Oops! Page not found.
+			</p>
+			<Link href="/">
+				<button
+					style={{ padding: "1rem 2rem", fontSize: "1rem", cursor: "pointer" }}
+				>
+					Go Back Home
+				</button>
+			</Link>
+		</div>
+	);
+}
