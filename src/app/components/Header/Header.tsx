2df4883a658462c335b46fe1f5bb2bbc9205c0ca
Stepan Dordiai <stepandordiai@gmail.com>
2025-10-06 09:24:07 +0200

    Update Header

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,15 +1,35 @@
 "use client";
 
-import { useEffect, useRef } from "react";
-import logoImg from "./../../../../public/heeeyooo-studio-logo-white-v1.svg";
+import { useEffect, useState } from "react";
 import workData from "./../../data/work-data.json";
-import styles from "./Header.module.scss";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import logoImg from "./../../../../public/heeeyooo-studio-logo-white-v1.svg";
+import styles from "./Header.module.scss";
 
 const Header = () => {
 	// TODO:
 	const pathname = usePathname();
+
+	const [isMenuActive, setIsMenuActive] = useState(false);
+
+	useEffect(() => {
+		setIsMenuActive(false);
+	}, [pathname]);
+
+	useEffect(() => {
+		const closeMenuOnEsc = (e: KeyboardEvent) => {
+			if (e.key === "Escape") {
+				setIsMenuActive(false);
+			}
+		};
+
+		document.addEventListener("keydown", closeMenuOnEsc);
+
+		return () => {
+			document.removeEventListener("keydown", closeMenuOnEsc);
+		};
+	}, []);
 
 	useEffect(() => {
 		let prevScroll = 0;
@@ -32,48 +52,28 @@ const Header = () => {
 		};
 	}, []);
 
+	function toggleBurgerBtn() {
+		setIsMenuActive((prev) => !prev);
+	}
+
 	const inactiveLink = styles["header__nav-link"];
 	const activeLink = `${styles["header__nav-link"]} ${styles["header__nav-link--active"]}`;
 
-	const burgerBtn = useRef<HTMLDivElement>(null);
-	const header = useRef<HTMLElement>(null);
-	const burgerMenu = useRef<HTMLDivElement>(null);
-
-	function toggleBurgerBtn() {
-		if (burgerBtn.current?.classList.contains(styles["burger-btn--active"])) {
-			header.current?.classList.remove(styles["header--active"]);
-			burgerMenu.current?.classList.remove(styles["burger-menu--active"]);
-			burgerBtn.current?.classList.remove(styles["burger-btn--active"]);
-		} else {
-			header.current?.classList.add(styles["header--active"]);
-			burgerMenu.current?.classList.add(styles["burger-menu--active"]);
-			burgerBtn.current?.classList.add(styles["burger-btn--active"]);
-		}
-	}
-
-	useEffect(() => {
-		window.addEventListener("scroll", () => {
-			burgerBtn.current?.classList.remove(styles["burger-btn--active"]);
-			header.current?.classList.remove(styles["header--active"]);
-			burgerMenu.current?.classList.remove(styles["burger-menu--active"]);
-		});
-
-		document
-			.querySelectorAll(`.${styles["burger-menu__nav-link"]}`)
-			.forEach((link) => {
-				link.addEventListener("click", () => {
-					burgerBtn.current?.classList.remove(styles["burger-btn--active"]);
-					header.current?.classList.remove(styles["header--active"]);
-					burgerMenu.current?.classList.remove(styles["burger-menu--active"]);
-				});
-			});
-	}, []);
-
-	const inactiveMenuLink = styles["burger-menu__nav-link"];
-	const activeMenuLink = `${styles["burger-menu__nav-link"]} ${styles["burger-menu__nav-link--active"]}`;
+	const inactiveMenuLink = isMenuActive
+		? `${styles["menu__nav-link"]} ${styles["menu__nav-link--show"]}`
+		: `${styles["menu__nav-link"]}`;
+	const activeMenuLink = isMenuActive
+		? `${styles["menu__nav-link"]} ${styles["menu__nav-link--active"]} ${styles["menu__nav-link--show"]}`
+		: `${styles["menu__nav-link"]} ${styles["menu__nav-link--active"]}`;
 
 	return (
-		<header ref={header} className={styles.header}>
+		<header
+			className={
+				isMenuActive
+					? `${styles.header} ${styles["header--active"]}`
+					: styles.header
+			}
+		>
 			<div className={styles["header__inner"]}>
 				<Link className={styles["header__logo-link"]} href="">
 					<img
@@ -118,21 +118,35 @@ const Header = () => {
 					onClick={toggleBurgerBtn}
 					className={styles["burger-btn__container"]}
 				>
-					<div ref={burgerBtn} className={styles["burger-btn"]}></div>
+					<div
+						className={
+							isMenuActive
+								? `${styles["burger-btn"]} ${styles["burger-btn--active"]}`
+								: styles["burger-btn"]
+						}
+					></div>
 				</div>
 			</div>
-			<div ref={burgerMenu} className={styles["burger-menu"]}>
-				<nav className={styles["burger-menu__nav"]}>
-					<div className={styles["burger-menu__nav-item"]}>
+			<div
+				className={
+					isMenuActive
+						? `${styles["menu"]} ${styles["menu--active"]}`
+						: styles["menu"]
+				}
+			>
+				<nav className={styles["menu__nav"]}>
+					<div className={styles["menu__nav-item"]}>
 						<Link
+							onClick={() => setIsMenuActive(false)}
 							className={pathname === "/" ? activeMenuLink : inactiveMenuLink}
 							href="/"
 						>
 							Home
 						</Link>
 					</div>
-					<div className={styles["burger-menu__nav-item"]}>
+					<div className={styles["menu__nav-item"]}>
 						<Link
+							onClick={() => setIsMenuActive(false)}
 							className={
 								pathname === "/about" ? activeMenuLink : inactiveMenuLink
 							}
@@ -141,8 +155,9 @@ const Header = () => {
 							About
 						</Link>
 					</div>
-					<div className={styles["burger-menu__nav-item"]}>
+					<div className={styles["menu__nav-item"]}>
 						<Link
+							onClick={() => setIsMenuActive(false)}
 							className={
 								pathname === "work" ? activeMenuLink : inactiveMenuLink
 							}
@@ -152,8 +167,9 @@ const Header = () => {
 							<span className="work-qty">{workData.length}</span>
 						</Link>
 					</div>
-					<div className={styles["burger-menu__nav-item"]}>
+					<div className={styles["menu__nav-item"]}>
 						<Link
+							onClick={() => setIsMenuActive(false)}
 							className={
 								pathname === "/contacts" ? activeMenuLink : inactiveMenuLink
 							}
