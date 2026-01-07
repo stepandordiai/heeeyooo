70931a6cbb7326ec664eda9c416637bd6b154836
Stepan Dordiai <stepandordiai@gmail.com>
2026-01-07 22:50:16 +0100

    Update loading component

--- src/app/[locale]/loading.tsx
+++ src/app/[locale]/loading.tsx
@@ -1,5 +1,23 @@
+import Image from "next/image";
+
 const Loading = () => {
-	return <p>Loading...</p>;
+	return (
+		<div
+			style={{
+				height: "100svh",
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<Image
+				src="/heeeyooo-studio-logo-white-v1.svg"
+				width={100}
+				height={100}
+				alt="heeeyooo studio logo"
+			/>
+		</div>
+	);
 };
 
 export default Loading;
