efedefceb0371bb546ee91402dfee0978146107c
Stepan Dordiai <stepandordiai@gmail.com>
2026-03-12 15:40:59 +0100

    feat: update Technologies component

--- src/app/components/home/Technologies/Technologies.tsx
+++ src/app/components/home/Technologies/Technologies.tsx
@@ -1,45 +1,40 @@
 import { getTranslations } from "next-intl/server";
+import WordLine from "../../WordLine/WordLine";
 import styles from "./Technologies.module.scss";
+
+const technologies = [
+	"Next.js",
+	"React",
+	"TypeScript",
+	"Sass",
+	"JavaScript",
+	"Node.js",
+	"CSS",
+	"HTML",
+	"express.js",
+	"Python",
+	"MongoDB",
+	"Figma",
+	"GitHub",
+	"Git",
+	"Bootstrap",
+	"PostgreSQL",
+];
 
 const Technologies = async () => {
 	const t = await getTranslations();
-	const technologiesData = [
-		{ name: "Next.js", fontClr: "#ffffff", bgClr: "#ffffff33" },
-		{ name: "React.js", fontClr: "#61dbfb", bgClr: "#61dcfb33" },
-		{ name: "TypeScript", fontClr: "#007acc", bgClr: "#007acc33" },
-		{ name: "Sass", fontClr: "#cc6699", bgClr: "#cc669933" },
-		{ name: "JavaScript", fontClr: "#f0db4f", bgClr: "#f0db4f33" },
-		{ name: "Node.js", fontClr: "#3c873a", bgClr: "#3c873a33" },
-		{ name: "CSS", fontClr: "#2965f1", bgClr: "#2965f133" },
-		{ name: "HTML", fontClr: "#e34c26", bgClr: "#e34c2633" },
-		{ name: "express.js", fontClr: "#ffffff", bgClr: "#ffffff33" },
-		{ name: "Python", fontClr: "#ffd43b", bgClr: "#ffd43b33" },
-		{ name: "MongoDB", fontClr: "#4db33d", bgClr: "#4db33d33" },
-		{ name: "Figma", fontClr: "#ff7262", bgClr: "#ff726233" },
-		{ name: "GitHub", fontClr: "#9cdaf1", bgClr: "#9cdaf133" },
-		{ name: "Git", fontClr: "#f1502f", bgClr: "#f1502f33" },
-		{ name: "Bootstrap", fontClr: "#563d7c", bgClr: "#563d7c33" },
-	];
 
 	return (
-		<div>
-			<h2 style={{ textAlign: "center" }}>{t("home.technologies")}</h2>
-			<div className={styles.technologies}>
-				{technologiesData.map((technology, index) => {
-					return (
-						<p
-							key={index}
-							style={{
-								color: technology.fontClr,
-								backgroundColor: technology.bgClr,
-							}}
-						>
-							{technology.name}
-						</p>
-					);
+		<section className={styles.section}>
+			<h2>
+				<WordLine text={t("home.technologies")} />
+			</h2>
+			<ul className={styles.technologies}>
+				{technologies.map((technology, index) => {
+					return <li key={index}>{technology}</li>;
 				})}
-			</div>
-		</div>
+			</ul>
+		</section>
 	);
 };
 
