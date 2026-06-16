4ad93f6c8e57e28b55e4e172160d8e0a6fcab5cf
Stepan Dordiai <stepandordiai@gmail.com>
2026-06-16 09:16:28 +0200

    feat: update Breadcrumbs

--- src/components/common/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/common/Breadcrumbs/Breadcrumbs.tsx
@@ -1,35 +1,34 @@
 import { getTranslations } from "next-intl/server";
 import { Link } from "@/i18n/navigation";
-import React from "react";
+import { Fragment } from "react";
+import ChevronRightIcon from "@/components/icons/ChevronRightIcon";
 import styles from "./Breadcrumbs.module.scss";
 
-interface Breadcrumb {
-	label: string;
-	path?: string;
-}
-
 type BreadcrumbsProps = {
-	links: Breadcrumb[];
+	links: {
+		label: string;
+		path?: string;
+	}[];
 };
 
 const Breadcrumbs = async ({ links }: BreadcrumbsProps) => {
 	const t = await getTranslations();
 
 	return (
-		<nav className="breadcrumbs" style={{ marginBottom: 20 }}>
+		<nav className={styles["breadcrumbs"]}>
 			<Link className={styles["breadcrumbs__link"]} href="/">
 				{t("homeTitle")}
 			</Link>
-			<span> &bull; </span>
+			<ChevronRightIcon />
 			{links.map((link, i) => {
 				const isLastLink = i === links.length - 1;
 				return link.path && !isLastLink ? (
-					<React.Fragment key={i}>
+					<Fragment key={i}>
 						<Link className={styles["breadcrumbs__link"]} href={link.path}>
 							{link.label}
 						</Link>
-						<span> &bull; </span>
-					</React.Fragment>
+						<ChevronRightIcon />
+					</Fragment>
 				) : (
 					<span key={i} style={{ color: "hsl(0, 0%, 50%)" }}>
 						{link.label}
