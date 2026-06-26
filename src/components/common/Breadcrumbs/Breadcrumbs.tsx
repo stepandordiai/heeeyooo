d12fb19e9377d234ab38b5be8856af412eca8139
Stepan Dordiai <stepandordiai@gmail.com>
2026-06-27 00:32:22 +0200

    feat: update Breadcrumbs

--- src/components/common/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/common/Breadcrumbs/Breadcrumbs.tsx
@@ -1,6 +1,5 @@
 import { getTranslations } from "next-intl/server";
 import { Link } from "@/i18n/navigation";
-import { Fragment } from "react";
 import ChevronRightIcon from "@/components/icons/ChevronRightIcon";
 import styles from "./Breadcrumbs.module.scss";
 
@@ -15,26 +14,32 @@ const Breadcrumbs = async ({ links }: BreadcrumbsProps) => {
 	const t = await getTranslations();
 
 	return (
-		<nav className={styles["breadcrumbs"]}>
-			<Link className={styles["breadcrumbs__link"]} href="/">
-				{t("homeTitle")}
-			</Link>
-			<ChevronRightIcon />
-			{links.map((link, i) => {
-				const isLastLink = i === links.length - 1;
-				return link.path && !isLastLink ? (
-					<Fragment key={i}>
-						<Link className={styles["breadcrumbs__link"]} href={link.path}>
-							{link.label}
-						</Link>
-						<ChevronRightIcon />
-					</Fragment>
-				) : (
-					<span key={i} style={{ color: "hsl(0, 0%, 50%)" }}>
-						{link.label}
-					</span>
-				);
-			})}
+		<nav className={styles.breadcrumbs}>
+			<ol className={styles["breadcrumbs-list"]}>
+				<li>
+					<Link className={styles["breadcrumbs__link"]} href="/">
+						{t("homeTitle")}
+					</Link>
+					<ChevronRightIcon size={14} />
+				</li>
+				{links.map((link, i) => {
+					const isLastLink = i === links.length - 1;
+					return link.path && !isLastLink ? (
+						<li key={i}>
+							<Link className={styles["breadcrumbs__link"]} href={link.path}>
+								{link.label}
+							</Link>
+							<ChevronRightIcon size={14} />
+						</li>
+					) : (
+						<li key={i}>
+							<span key={i} style={{ color: "hsl(0, 0%, 50%)" }}>
+								{link.label}
+							</span>
+						</li>
+					);
+				})}
+			</ol>
 		</nav>
 	);
 };
